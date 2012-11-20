5f26858a2b51a06ba03afc8b5e700435a890fccd
Avneet Singh <avneet723@gmail.com>
2012-11-20 13:37:31 -0500

    Porting BusTracker to Windows Phone 8.0 SDK

---
+++ BusTracker/BusTracker/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using BusTracker.Resources;
+
+namespace BusTracker
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
