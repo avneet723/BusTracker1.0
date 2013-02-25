8445363c0e9325a396f996b193951da8fba9989f
Jason Riddle <jr1285@vt.edu>
2013-02-25 01:26:27 -0500

    Added a button on the dininginfo.xaml page that shows which dining halls are open on 3/3/2013
    Next steps include implementing a way to select a dining hall, a way to choose a date, and html needs to be parsed for relevant info

--- BusTracker/DiningInfo.xaml.cs
+++ BusTracker/DiningInfo.xaml.cs
@@ -16,5 +16,36 @@ namespace BusTracker
         {
             InitializeComponent();
         }
+
+        public void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            string url = "https://secure.hosting.vt.edu/www.dining.vt.edu/hours/index.php?d=t";
+            var uri = new Uri(url, UriKind.Absolute);
+
+            string data = "d_month=" + 3 + "&d_day=" + 3 + "&d_year=" + 2013 + "&view=View+Date";
+
+            var wc = new WebClient();
+
+            wc.UploadStringCompleted += new UploadStringCompletedEventHandler(wc_UploadStringCompleted);
+
+            wc.Headers["Content-Type"] = "application/x-www-form-urlencoded";
+            wc.Headers["Content-Length"] = data.Length.ToString();
+            wc.UploadStringAsync(uri, "POST", data);
+
+        }
+
+        private void wc_UploadStringCompleted(object sender, UploadStringCompletedEventArgs e)
+        {
+
+            try
+            {
+                MessageBox.Show(e.Result);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
