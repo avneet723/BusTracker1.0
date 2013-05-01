f87f74763d6201749ec046c8e6ee1731fe3dfa6b
Cole Edwards <colee12@vt.edu>
2013-05-01 02:36:33 -0400

    Updated nutrition page

---
+++ BusTracker/NutritionItems.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusTracker
+{
+
+    class NutritionItems
+    {
+        public String Name { get; set; }
+        public String ServingSize { get; set; }
+        public String Calories { get; set; }
+        public String FatCalories { get; set; }
+        public String TotalFat { get; set; }
+        public String TotalFatDV { get; set; }
+        public String SatFat { get; set; }
+        public String SatFatDV { get; set; }
+        public String TransFat { get; set; }
+        public String Cholesterol { get; set; }
+        public String CholesterolDV { get; set; }
+        public String Sodium { get; set; }
+        public String SodiumDV { get; set; }
+        public String TotalCarbs { get; set; }
+        public String TotalCardDV { get; set; }
+        public String DietaryFiber { get; set; }
+        public String DietaryFiberDV { get; set; }
+        public String Sugar { get; set; }
+        public String Protein { get; set; }
+        public String VitaminA { get; set; }
+        public String VitaminC { get; set; }
+        public String Calcium { get; set; }
+        public String Iron { get; set; }
+
+    }
+
+
+}
